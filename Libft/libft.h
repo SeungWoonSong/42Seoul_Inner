0033a7626f3082f3c75e9d1014e5c9e4a3e613cc
SeungWoon Song <andrew960811@naver.com>
2022-08-02 17:02:30 +0900

    8.2.

---
+++ Libft/libft.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: susong <susong@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/01 16:14:12 by susong            #+#    #+#             */
+/*   Updated: 2022/08/02 16:55:18 by susong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <string.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+void	*ft_memset(void *s, int c, size_t n);
+size_t	ft_strlen(const char *s);
+
+#endif

e9dded8fa9653a1ce879de56f83e4229f75be262
SeungWoon Song <andrew960811@naver.com>
2022-08-03 12:32:19 +0900

    First in 8.3.

--- Libft/ft_strchr.c
+++ Libft/ft_strchr.c
@@ -1,32 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: susong <susong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/01 16:14:12 by susong            #+#    #+#             */
-/*   Updated: 2022/08/02 17:13:17 by susong           ###   ########.fr       */
+/*   Created: 2022/08/02 17:13:31 by susong            #+#    #+#             */
+/*   Updated: 2022/08/02 17:13:45 by susong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <string.h>
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
+char	*ft_strchr(const char *s, int c)
+{
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-char	*ft_strchr(const char *s, int c);
-void	*ft_memset(void *s, int c, size_t n);
-size_t	ft_strlen(const char *s);
-
-#endif
+}

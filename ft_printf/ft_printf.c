094fa7bceded5b6ea1f3baa2487d77f20a993563
SeungWoon Song <andrew960811@naver.com>
2022-08-22 13:54:46 +0900

    check

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: susong <susong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 12:57:45 by susong            #+#    #+#             */
-/*   Updated: 2022/08/18 14:12:38 by susong           ###   ########.fr       */
+/*   Updated: 2022/08/19 15:49:30 by susong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,24 @@ int	ft_printf(const char *s, ...)
 	va_list	ap;
 	int		index;
 	int		print_size;
+	int		past_size;
 
 	print_size = 0;
 	index = 0;
 	va_start(ap, s);
 	while (s[index])
 	{
+		past_size = print_size;
 		if (s[index] != '%')
 			print_size += write(1, &s[index], 1);
 		else
 			print_size += ft_write_arg(&s[index++], &ap);
 		index++;
+		if (past_size > print_size)
+		{
+			va_end(ap);
+			return (-1);
+		}
 	}
 	va_end(ap);
 	return (print_size);
--- ft_printf/test.c
+++
@@ -1,6 +0,0 @@
-#include "inc/ft_printf.h"
-
-int main()
-{
-	ft_printf("\n$%x$\n", (unsigned int)3735929054);
-}

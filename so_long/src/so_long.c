f224f77308024d77d3379059023215213446adc5
SeungWoon Song <andrew960811@naver.com>
2022-09-24 21:12:44 +0900

    Studing

--- so_long/src/so_long.c
+++ so_long/src/so_long.c
@@ -6,29 +6,51 @@
 /*   By: susong <susong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 16:15:23 by susong            #+#    #+#             */
-/*   Updated: 2022/09/24 20:13:45 by susong           ###   ########.fr       */
+/*   Updated: 2022/09/24 21:07:38 by susong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/so_long.h"
+// #include "../include/so_long.h"
+// #include "../include/study.h"
+#include "mlx.h"
 
-int main(int argc, char **argv)
+
+int	main(void)
 {
-	t_map *map;
+	void	*mlx;
+	void	*win;
+	void	*img;
+	int		img_width;
+	int		img_height;
 
-	if(argc <= 1)
-		print_error(1); //First Error_Nomap #1
-	map = check_map(argv[1]);
-	if(!map)
-		print_error(1);
-	printf("%s\n",map->map_data);
-	printf("main ended");
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, 1920, 1080, "putImage");
+	img = mlx_xpm_file_to_image(mlx, "./img/spider.xpm", &img_width, &img_height);
+	mlx_put_image_to_window(mlx, win, img, 50, 50);
+	mlx_loop(mlx);
+	return (0);
 }
 
 
-void print_error(int flag)
-{
-	printf("Error\n");
 
-	exit(1);
-}
+
+// int main(int argc, char **argv)
+// {
+// 	t_map *map;
+
+// 	if(argc <= 1)
+// 		print_error(1); //First Error_Nomap #1
+// 	map = check_map(argv[1]);
+// 	if(!map)
+// 		print_error(1);
+// 	printf("%s\n",map->map_data);
+// 	printf("main ended");
+// }
+
+
+// void print_error(int flag)
+// {
+// 	printf("Error\n");
+
+// 	exit(1);
+// }
